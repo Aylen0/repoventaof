50f92b105fe169fd463de28af06fafd31d2d277a
FERNANDOR21 <fernandorodriguez.37421@gmail.com>
2025-06-19 13:47:42 -0500

    se subio exitosamente la conexion y estilo

--- ModConexion/conexion.php
+++ ModConexion/conexion.php
@@ -1,1 +1,15 @@
-<h1>Hola</h1>
+<?php
+$usuario = "postgres";
+$clave = "123";
+$basedatos = "ventasdb";
+$host = "l10.0.221.105";
+$puerto = "5432";
+
+try {
+    $con = new PDO("pgsql:host=$host;port=$puerto;dbname=$basedatos", $usuario, $clave);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+    $con = null; 
+}
+?>
